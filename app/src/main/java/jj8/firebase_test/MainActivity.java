ff2fa5b21e1e17ae68fccd92b2057517fb9b324c
joelj <jo>
2016-11-27 17:50:25 -0800

    moved login stuff  to dedicated activity

--- app/src/main/java/jj8/firebase_test/MainActivity.java
+++ app/src/main/java/jj8/firebase_test/MainActivity.java
@@ -36,11 +36,8 @@ public class MainActivity extends AppCompatActivity  implements
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private String mUserId;
 
-    protected EditText emailEditText;
-    protected EditText passwordEditText;
-    protected Button logInButton;
-    protected TextView signUpTextView;
 
 
 
@@ -51,151 +48,30 @@ public class MainActivity extends AppCompatActivity  implements
 
         // Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
-
-        signUpTextView = (TextView) findViewById(R.id.signUpText);
-        emailEditText = (EditText) findViewById(R.id.emailField);
-        passwordEditText = (EditText) findViewById(R.id.passwordField);
-        logInButton = (Button) findViewById(R.id.loginButton);
-
-        signUpTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-
-                email = email.trim();
-                password = password.trim();
-
-                if (email.isEmpty() || password.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setMessage(
-                            R.string.login_error_message)
-                            .setTitle(R.string.login_error_title)
-                            .setPositiveButton(android.R.string.ok, null);
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                } else {
-                    mFirebaseAuth.signInWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                        startActivity(intent);
-                                    } else {
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                                        builder.setMessage(task.getException().getMessage())
-                                                .setTitle(R.string.login_error_title)
-                                                .setPositiveButton(android.R.string.ok, null);
-                                        AlertDialog dialog = builder.create();
-                                        dialog.show();
-                                    }
-                                }
-                            });
-                }
-            }
-        });
-
-
-
-
-    findViewById(R.id.sign_in_button).setOnClickListener(this);
-        mStatusTextView=(TextView)findViewById(R.id.text1);
-
-
-
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-// options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        // Initialize Firebase Auth
-        mFirebaseAuth = FirebaseAuth.getInstance();
+    // Initialize Firebase Auth and Database Reference
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-
+       // mDatabase = FirebaseDatabase.getInstance().getReference();
 
         if (mFirebaseUser == null) {
             // Not logged in, launch the Log In activity
-         //   loadLogInView();
-        }
-    }
-
-   // private void loadLogInView() {
-      //  Intent intent = new Intent(this, LogInActivity.class);
-       // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      //  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-     //   startActivity(intent);
-   // }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
-        }
-    }
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-       //mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-
-    /*
-    You can also get the user's email address with getEmail,
-     the user's Google ID (for client-side use) with getId,
-     and an ID token for the user with with getIdToken.
-     If you need to pass the currently signed-in user to a backend server,
-     send the ID token to your backend server and validate the token on
-     the server.
-     */
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-            updateUI(true);
+            loadLogInView();
         } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false);
+            mUserId = mFirebaseUser.getUid();
         }
     }
 
-    private void updateUI(boolean b) {
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+       startActivity(intent);
     }
 
+
+    @Override
+    public void onClick(View view) {
+
+    }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
