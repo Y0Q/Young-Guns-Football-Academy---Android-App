9224436afe303467ad7157fadd4dd14e23ab7db1
joelj <jo>
2016-12-06 17:28:27 -0800

    Added Photo gallery

---
+++ app/src/main/java/jj8/firebase_test/Event_DataBase.java
@@ -0,0 +1,33 @@
+package jj8.firebase_test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by joelj on 12/6/2016.
+ */
+
+public class Event_DataBase extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    private String mUserId;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+
+        // Initialize Firebase Auth and Database Reference
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserId = mFirebaseUser.getUid();
+    }
+}
